c7471144c827175da7d07d58e1a8699cd6ef0e50
A Product Technologies <38760381+iimbanglore@users.noreply.github.com>
2026-01-08 00:38:28 +0530

    Add App.js

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import { StatusBar, StyleSheet, SafeAreaView, Platform } from 'react-native';
 import { WebView } from 'react-native-webview';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import { Ionicons } from '@expo/vector-icons';
 
-const Tab = createBottomTabNavigator();
+const WEBSITE_URL = 'https://example.com';
 
-const navItems = [{"id":"1","label":"Home","url":"/home","icon":"home"},{"id":"1767796630011","label":"Account","url":"/login","icon":"user"},{"id":"1767796631113","label":"Import","url":"/import-video","icon":"settings"}];
-
-function WebViewScreen({ url }) {
+function WebViewScreen({ url = WEBSITE_URL }) {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="dark-content" />
@@ -27,33 +22,8 @@ function WebViewScreen({ url }) {
   );
 }
 
-function AppNavigator() {
-  return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          const item = navItems.find(n => n.label === route.name);
-          return <Ionicons name={item?.icon || 'home'} size={size} color={color} />;
-        },
-      })}
-    >
-      {navItems.map((item, index) => (
-        <Tab.Screen 
-          key={index}
-          name={item.label} 
-          children={() => <WebViewScreen url={"https://play.uben.in/" + item.url} />}
-        />
-      ))}
-    </Tab.Navigator>
-  );
-}
-
 export default function App() {
-  return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
-  );
+  return <WebViewScreen />;
 }
 
 const styles = StyleSheet.create({
